1d9b8d3375b21534e8fbd2adb0187291a261dd4a
brandon gibson <you@example.com>
2023-05-05 19:09:59 -0400

    second commit

---
+++ src/state.js
@@ -0,0 +1,271 @@
+const states = [
+    {
+        id: 1,
+        name: "Alabama",
+        value: "al"
+    },
+    {   
+        id: 2,
+        name: "Alaska",
+        value:"ak"
+    },
+    {   
+        id: 3,
+        name: "Arizona",
+        value:"az"
+    },
+    {   
+        id: 4,
+        name: "Arkansas",
+        value: "ar"
+    },
+    {   
+        id: 5,
+        name: "California",
+        value:"ca"
+    },
+    {   
+        id: 6,
+        name: "Colorado",
+        value: "co"
+    },
+    {   
+        id: 7,
+        name: "Connecticut",
+        value: "ct"
+    },
+    {   
+        id: 8,
+        name: "Delaware",
+        value: "de"
+    },
+    {   
+        id: 9,
+        name: "District of Columbia",
+        value: "dc"
+    },
+    {   
+        id: 10,
+        name: "Florida",
+        value: "fl"
+    },
+    {   
+        id: 11,
+        name: "Gerogia",
+        value: "ga"
+    },
+    {   
+        id: 12,
+        name: "Hawaii",
+        value: "hi"
+    },
+    {   
+        id: 13,
+        name: "Idaho",
+        value: "id"
+    },
+    {
+        id: 14,
+        name: "Illinois",
+        value: "il"
+    },
+    {
+        id: 15,
+        name: "Indiana",
+        value: "in"
+    },
+    {
+        id: 16,
+        name: "Iowa",
+        value: "ia"
+    },
+    {
+        id: 17,
+        name: "Kansas",
+        value: "ks"
+    },
+    {
+        id: 18,
+        name: "Kentucky",
+        value: "ky"
+    },
+    {
+        id: 19,
+        name: "Louisiana",
+        value: "la"
+    },
+    {
+        id: 20,
+        name: "Maine",
+        value: "me"
+    },
+    {
+        id: 21,
+        name: "Maryland",
+        value: "md"
+    },
+    {
+        id: 22,
+        name: "Massachusetts",
+        value: "ma"
+    },
+    {
+        id: 23,
+        name: "Michigan",
+        value: "mi"
+    },
+    {
+        id: 24,
+        name: "Minnesota",
+        value: "mn"
+    },
+    {
+        id: 25,
+        name: "Mississippi",
+        value: "ms"
+    },
+    {
+        id: 26,
+        name: "Missouri",
+        value: "mo"
+    },
+    {
+        id: 27,
+        name: "Montana",
+        value: "mt"
+    },
+    {
+        id: 28,
+        name: "Nebraska",
+        value: "ne"
+    },
+    {
+        id: 29,
+        name: "Nevada",
+        value: "nv"
+    },
+    {
+        id: 30,
+        name: "New Hampshire",
+        value: "nh"
+    },
+    {
+        id: 31,
+        name: "New Jersey",
+        value: "nj"
+    },
+    {
+        id: 32,
+        name: "New Mexico",
+        value: "nm"
+    }, 
+    {
+        id: 33,
+        name: "New York",
+        value: "ny"
+    }, 
+    {
+        id: 34,
+        name: "North Carolina",
+        value: "nc"
+    },
+    {
+        id: 35,
+        name: "North Dakota",
+        value: "nd"
+    },
+    {
+        id: 36,
+        name: "Ohio",
+        value: "oh"
+    },
+    {
+        id: 37,
+        name: "Oklahoma",
+        value: "ok"
+    },
+    {
+        id: 38,
+        name: "Oregon",
+        value: "or"
+    },
+    {
+        id: 39,
+        name: "Pennsylvania",
+        value: "pa"
+    },
+    {
+        id: 40,
+        name: "Puerto Rico",
+        value: "pr"
+    },
+    {
+        id: 41,
+        name: "Rhode Island",
+        value: "ri"
+    },
+    {
+        id: 42,
+        name: "South Carolina",
+        value: "sc"
+    },
+    {
+        id: 43,
+        name: "South Dakato",
+        value: "sd"
+    },
+    {
+        id: 44,
+        name: "Tennessee",
+        value: "tn"
+    },
+    {
+        id: 45,
+        name: "Texas",
+        value: "tx"
+    },
+    {
+        id: 46,
+        name: "Utah",
+        value: "ut"
+    },
+    {
+        id: 47,
+        name: "Vermont",
+        value: "vt"
+    },
+    {
+        id: 48,
+        name: "Virginia",
+        value: "va"
+    },
+    {
+        id: 49,
+        name: "Virgin Islands",
+        value: "vi"
+    },
+    {
+        id: 50,
+        name: "Washington",
+        value: "wa"
+    },
+    {
+        id: 51,
+        name: "West Virginia",
+        value: "wv"
+    },
+    {
+        id: 52,
+        name: "Wisconsin",
+        value: "wi"
+    },
+    {
+        id: 53,
+        name: "Wyoming",
+        value: "wy"
+    }
+    
+
+]
+
+export default states
